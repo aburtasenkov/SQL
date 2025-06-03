6a02d7372c5e7a824613e331a3a5faec766e8a43
aburtasenkov <59296596+aburtasenkov@users.noreply.github.com>
2025-06-04 01:40:56 +0200

    Initial Commit

---
+++ token.cpp
@@ -0,0 +1,24 @@
+#include "token.hpp"
+
+std::string argumentsToString(int argc, char ** argv) 
+// convert command line arguments vector into input stream 
+{
+  std::ostringstream stream;
+  for (int i = 1; i < argc; ++i) {
+    stream << argv[i] << " ";
+  }
+  return stream.str();
+}
+
+std::string getToken(std::istream& is) 
+// return one lowercase word from is
+{
+  std::string token;
+  is >> token;
+  std::transform(token.begin(), token.end(), token.begin(), 
+    [](unsigned char ch){
+      return std::tolower(ch);          
+    }
+  );
+  return token;
+}
---
+++ token.hpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+
+std::string argumentsToString(int argc, char ** argv);
+std::string getToken(std::istream& is);
