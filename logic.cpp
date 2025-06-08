18444d577b4732dc6aecc632468f809b6902731e
aburtasenkov <aburtasenkov@gmail.com>
2025-06-08 19:38:26 +0200

    Moved SQL logic functions into a header file

---
+++ logic.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <filesystem>
+
+#include "logic.hpp"
+#include "constants.hpp"
+#include "token.hpp"
+#include "database.hpp"
+
+// Database currentDatabase;
+
+void create(std::istream& is)
+// SQL Create Operation
+{
+  Object obj;
+  is >> obj;
+  switch (obj) {
+    case Object::None: 
+      std::cerr << "create::BadSyntaxError\n";
+      break;
+    case Object::Table:
+      // TODO CREATE TABLE
+      break;
+    case Object::Database:
+      createDatabase(is);
+      break;
+  }
+}
+
+Database createDatabase(std::istream& is)
+// Create New Database
+{
+  std::string databaseName = getToken(is);
+  if (std::filesystem::exists(DatabasesDirectory/databaseName)) throw std::runtime_error("createDatabase::DatabaseAlreadyExistsError");
+  std::filesystem::create_directory(DatabasesDirectory/databaseName);
+  std::cout << "Database " << databaseName << " created.\n";
+  return Database("pibbles");
+}
+
+void useDatabase(std::istream& is) 
+// Change Current Database in Use
+{
+  // TODO USE DATABASE
+  // std::string databaseName = getToken(is);
+  // if (!std::filesystem::exists(DatabasesDirectory/databaseName)) throw std::runtime_error("useDatabase::MissingDatabaseError");
+  // currentDatabase = databaseName;
+  // std::cout << "Using database " << currentDatabase << ".\n";
+}
---
+++ logic.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <filesystem>
+#include <iostream>
+
+#include "database.hpp"
+
+// extern Database currentDatabase;
+
+void create(std::istream&); // SQL Create Operation 
+
+Database createDatabase(std::istream&); // Create New Database
+void useDatabase(std::istream&); // Change Current Database in Use
