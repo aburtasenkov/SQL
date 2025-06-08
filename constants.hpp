8a39ec7c619f6c59a871c97f739b1f97053104f8
aburtasenkov <aburtasenkov@gmail.com>
2025-06-08 18:54:47 +0200

    Added comments

--- constants.hpp
+++ constants.hpp
@@ -3,8 +3,11 @@
 #include <filesystem>
 #include <string>
 
-extern const std::filesystem::path DatabasesDirectory;
-extern const std::string FileExtension;
-extern const char FieldDelimiter;
+extern const std::filesystem::path DatabasesDirectory;  // File Directory
+extern const std::string FileExtension; // Self Explanatory
+
+// Characters for Parsing the Tables
+extern const char FieldDelimiter; // Delimiter For a Field in .csv Table of a Database
+// Delimiters for Type of a Field 
 extern const char TypeOpener;
 extern const char TypeCloser;
