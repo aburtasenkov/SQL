e1123a3efd1e770275cb6a7281259c7e17fecee0
aburtasenkov <aburtasenkov@gmail.com>
2025-06-08 19:02:10 +0200

    Added comments and upgraded readability

--- database.cpp
+++ database.cpp
@@ -10,31 +10,37 @@
 Database::Database(std::string databaseName)
   :_databaseName(databaseName)
 {
+  // If Database Directory Does Not Exist Yet - Create It 
   if (!std::filesystem::exists(DatabasesDirectory)) {
     std::filesystem::create_directory(DatabasesDirectory);
     std::cout << "Database Directory Does Not Exist Yet...\n"
               << "Database Directory Created.\n";
   }
 
+  // If Database Does Not Exist Yet - Create It and Return
   if (!std::filesystem::exists(DatabasesDirectory / databaseName)) {
     std::filesystem::create_directory(DatabasesDirectory / databaseName);
     std::cout << "Database '" << databaseName << "' Was Created.\n";
     return;
   }
-
-  for (auto const& dirEntry : std::filesystem::directory_iterator{DatabasesDirectory / _databaseName}) {
-    _tables.push_back(dirEntry.path().stem());
+  
+  // Code For Existing Databases
+  // Read All Tables
+  for (const auto& dirEntry : std::filesystem::directory_iterator{DatabasesDirectory / this->_databaseName}) {
+    this->_tables.push_back(dirEntry.path().stem());
   }
 }
 
 Database::~Database() {}
 
 void Database::createTable(std::string tableName) {
-  if (std::find(_tables.begin(), _tables.end(), tableName) != _tables.end()) {
+  // If Already Exists - Nothing to Create
+  if (std::find(this->_tables.begin(), this->_tables.end(), tableName) != this->_tables.end()) {
     std::cout << "Table '" << tableName << "' Already Exists.\n";
     return;
   }
 
-  std::ofstream ofs {DatabasesDirectory / _databaseName / (tableName + FileExtension)};
+  // TODO - Initializer List for Fields
+  std::ofstream ofs {DatabasesDirectory / this->_databaseName / (tableName + FileExtension)};
   std::cout << "Table '" << tableName << "' Was Created.\n";
 }
