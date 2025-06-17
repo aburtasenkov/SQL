2a2f997dddecfc39d282d9288c1e0509db5cc47f
aburtasenkov <aburtasenkov@gmail.com>
2025-06-17 12:22:44 +0200

    Added scope

--- source/constants.cpp
+++ source/constants.cpp
@@ -3,8 +3,8 @@
 #include <filesystem>
 #include <string>
 
-const std::filesystem::path DatabasesDirectory = "databases";
-const std::string FileExtension = ".csv";
-const char FieldDelimiter = ',';
-const char ParameterOpener = '(';
-const char ParameterCloser = ')';
+const std::filesystem::path Constant::DatabasesDirectory = "databases";
+const std::string Constant::FileExtension = ".csv";
+const char Constant::FieldDelimiter = ',';
+const char Constant::ParameterOpener = '(';
+const char Constant::ParameterCloser = ')';
--- source/constants.hpp
+++ source/constants.hpp
@@ -3,11 +3,13 @@
 #include <filesystem>
 #include <string>
 
-extern const std::filesystem::path DatabasesDirectory;  // File Directory
-extern const std::string FileExtension; // Self Explanatory
-
-// Characters for Parsing the Tables
-extern const char FieldDelimiter; // Delimiter For a Field in .csv Table of a Database
-// Delimiters for Parameters in SQL Syntax 
-extern const char ParameterOpener;
-extern const char ParameterCloser;
+namespace Constant{
+  extern const std::filesystem::path DatabasesDirectory;  // File Directory
+  extern const std::string FileExtension; // Self Explanatory
+  
+  // Characters for Parsing the Tables
+  extern const char FieldDelimiter; // Delimiter For a Field in .csv Table of a Database
+  // Delimiters for Parameters in SQL Syntax 
+  extern const char ParameterOpener;
+  extern const char ParameterCloser;
+};
